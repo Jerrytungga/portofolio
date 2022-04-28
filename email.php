f8a6daee46c69ae0d9b59cb38c11f85a9a5286d3
jerrychristiangedeontungga41@gmail.com <jerrychristiangedeontungga41@gmail.com>
2022-04-28 18:11:03 +0700

    pesa

--- email.php
+++ email.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_POST['sent'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $pesan = $_POST['message'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $pesan = $_POST['message'];
 
-    $mailto = "jerrychristiangedeontungga41@gmail.com";
-    $headers = "From: " . $email;
-    $text = " Hai Jerry Christian, Anda mendapatkan pesan email dari " . $name . ".\n\n" . $pesan;
-    mail($mailto, $subject, $text, $headers);
-    header('location: kontak.php');
+  $mailto = "jerrychristian@flats.id";
+  $headers = "From: " . $email;
+  $text = " Hai Jerry Christian, Anda mendapatkan pesan email dari " . $name . ".\n\n" . $pesan;
+  mail($mailto, $subject, $text, $headers);
+  header('location: kontak.php');
 }
