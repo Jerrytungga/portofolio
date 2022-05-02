1d5044cf0134af62985e31ee1b0007914c068f38
jerrychristiangedeontungga41@gmail.com <jerrychristiangedeontungga41@gmail.com>
2022-05-02 18:42:58 +0700

    pesan

--- admin/sertifikasi.php
+++ admin/sertifikasi.php
@@ -40,7 +40,7 @@ $tb_sertifikasi = mysqli_query($conn, "SELECT * FROM `tb_sertifikasi` ");
 </head>
 
 <body>
-
+  <div class="elfsight-app-7964101c-18ea-4d43-8869-e4f4164300e9"></div>
   <main>
     <div class="container py-4">
       <?php
@@ -97,6 +97,7 @@ $tb_sertifikasi = mysqli_query($conn, "SELECT * FROM `tb_sertifikasi` ");
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
