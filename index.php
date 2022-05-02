42cb735596f62a675488c430613cb1184f4bb127
jerrychristiangedeontungga41@gmail.com <jerrychristiangedeontungga41@gmail.com>
2022-05-02 20:01:39 +0700

    pesan

--- index.php
+++ index.php
@@ -19,17 +19,17 @@ include 'head.php';
             <!-- <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p> -->
         </div>
         <br>
-        <h3>Artikel Terbaru</h3>
-        <hr>
+        <!-- <h3>Artikel Terbaru</h3>
+        <hr> -->
 
-        <div class="card">
+        <!-- <div class="card">
             <h5 class="card-header">Featured</h5>
             <div class="card-body">
                 <h5 class="card-title">Special title treatment</h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-        </div>
+        </div> -->
 
 
         <?php
